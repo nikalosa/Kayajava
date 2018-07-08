321f6aef19fe14bb104780f4e28037932b1638e6
beqakd <39763019+beqakd@users.noreply.github.com>
2018-07-09 00:36:04 +0400

    validation without javascript. register class finished + achivment inner class for each user

--- src/functionalPack/User.java
+++ src/functionalPack/User.java
@@ -1,20 +1,29 @@
 package functionalPack;
 
 
+import javafx.util.Pair;
+
 import java.security.MessageDigest;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class User {
 
     private static MessageDigest messageDigest;
     private String mail;
     private String password;
     private String userName;
+    private String confirmPass;
 
-    public User(String mail, String password, String userName){
+    public User(String mail, String password, String userName,String confirmPass){
         this.mail = mail;
-        this.password = makeHashCode(password);
+        this.password = password;
         this.userName = userName;
+        this.confirmPass = confirmPass;
     }
 
     public boolean  checkPassword(String pass){
@@ -60,12 +69,16 @@ public class User {
         return mail;
     }
 
-    public String validUser(){
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public ArrayList<String> validUser(){
+        ArrayList<String> arr = new ArrayList<>();
         String password = this.password;
         if(password.length()<8){
-            return "Too short password";
+            arr.add("Too short password");
         }
-        boolean numb=false, sign=false, bigLetters=false, smallLetters=false;
+        boolean numb=false, bigLetters=true, smallLetters=false;
         for(int i=0;i<password.length();i++){
             char ch = password.charAt(i);
             if(ch>='a' && ch<='z'){
@@ -74,22 +87,86 @@ public class User {
                 bigLetters = true;
             }else if(ch>='0' && ch<='9'){
                 numb = true;
-            }else{
-                sign = true;
             }
         }
         if(!numb){
-            return "No numbers in your password, use them for your safety";
-        }
-        if(!sign){
-            return "No sign symbols in your password, use them for your safety";
+
+            arr.add("No numbers in your password, use them for your safety");
         }
         if(!bigLetters){
-            return "No big letters in your password, use them for your safety";
+            arr.add("No big letters in your password, use them for your safety");
         }
         if(!smallLetters){
-            return "No small letters in your password, use them for your safety";
+            arr.add("No small letters in your password, use them for your safety");
         }
-        return "valid";
+//      ///////////mail
+
+        if(mail ==null || mail.isEmpty()|| !VALID_EMAIL_ADDRESS_REGEX.matcher(mail).matches()) {
+            arr.add("No small letters in your password, use them for your safety");
+        }
+
+
+        ///confirm pass.
+        if(!confirmPass.equals(this.password)) {
+            arr.add("Password does not matches.");
+
+        }
+        ///username
+        if(this.userName.equals("")) {
+            arr.add("Empty username, please enter.");
+        }
+        return arr;
     }
+
+
+    ///inner class of Achievements.
+    private class Achievements {
+        HashMap<String, Pair<String, Boolean> > map;
+        String [] arr = {"Amateur Author","Prolific Author","Prodigious Author",
+                "Quiz Machine","I am the Greatest","Practice Makes Perfect"};
+        String [] arrDisc = {"The user created a quiz.","The user created five quizzes.",
+                    "The user created ten quizzes","The user took ten quizzes.",
+                    "The user had the highest score on a quiz."};
+
+        Boolean[] boolArr;
+        public Achievements() {
+            map = new HashMap<String, Pair<String, Boolean> >();
+            boolArr = new Boolean[arr.length];
+            defaultArr();
+            fillHashMap();
+        }
+
+//        Pair<Integer, String> pair = new Pair<>(1, "One");
+//        Integer key = pair.getKey();
+//        String value = pair.getValue();
+
+        public void AchievementDone(String AchievementName) {
+            Pair<String, Boolean> pair = map.get(AchievementName);
+            if (pair != null) {
+                Pair<String, Boolean> newPair = new Pair<>(pair.getKey(), true);
+                map.replace(AchievementName, newPair);
+            }
+        }
+
+        private void fillHashMap() {
+            for(int i =0;i<arr.length;i++) {
+                Pair<String, Boolean> pair = new Pair<>(arrDisc[i], false);
+                map.put(arr[i],pair);
+            }
+        }
+        private void defaultArr() {
+            for(int i =0; i <boolArr.length;i++) {
+                boolArr[i] = false;
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
 }
