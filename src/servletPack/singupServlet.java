1853298d4ed446110357c624c562130a1ea95e68
beqakd <39763019+beqakd@users.noreply.github.com>
2018-07-09 01:39:19 +0400

    sign up servlet url changed and index signup tracked

--- src/servletPack/singupServlet.java
+++ src/servletPack/singupServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "singupServlet")
+@WebServlet(name = "singupServlet", urlPatterns = {"/singupServlet"})
 public class singupServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatch = request.getRequestDispatcher("Registration.jsp");
